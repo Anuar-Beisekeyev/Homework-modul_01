bafdaec9fb5079fc7262ba997b2057083a9b71b0
Bejs_vf89@student.itstep.kz <Bejs_vf89@student.itstep.kz>
2020-12-04 23:49:08 +0600

    добавил репозиторий домашнего задания по первому модулю

---
+++ HomeworkM_01/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace HomeworkM_01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Бейсекеев Ануар");
+
+            Console.WriteLine("Введите число 1:");
+
+            double firstValue = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Введите число 2:");
+
+            double secondValue = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Введите число 3:");
+
+            double thirdValue = double.Parse(Console.ReadLine());
+
+            double resultSum = firstValue + secondValue + thirdValue;
+
+            Console.WriteLine("Сумма введенных чисел = " + resultSum);
+
+
+        }
+    }
+}
